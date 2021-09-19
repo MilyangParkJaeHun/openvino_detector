4e910c3bca9454284613e3b0c849e8464fbaf865
MilyangParkJaeHun <jh9277@naver.com>
2021-09-19 13:38:24 +0000

    add comments

--- detector.py
+++ detector.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+"""
+    detector.py
+
+    Author: Park Jaehun
+    Refactoring: Park Jaehun , 2021.09.19
+"""
 
 import os
 import cv2
@@ -30,8 +35,15 @@ if __name__ == "__main__":
     display = args.display
     phase = args.phase
 
+    # for calculate FPS
+    total_time = 0 
+    total_frame = 0
+
+    # only detect person
     label_map = [''] * 90
     label_map[0] = 'person'
+
+    # create model parser according to model type[yolo/ssd]
     if args.model_type == "yolo":
         model = Yolo()
     elif args.model_type == "ssd":
@@ -40,19 +52,17 @@ if __name__ == "__main__":
         print("Supports ssd or yolo as detection models. \n Choose between ssd and yolo!!!")
         sys.exit(1)
 
+    # create Detector instance
     detector = OpenvinoDet(model, args.model_path, args.device, label_map, args.prob_threshold)
 
     if not os.path.exists('output'):
         os.makedirs('output')
     pattern = os.path.join(args.img_path, phase, '*', 'img1')
 
-
-    total_time = 0 
-    total_frame = 0
     for img_dir in glob.glob(pattern):
+        # clear detection results buffer 
         detector.clear()
 
-        frame_id = 0
         seq = img_dir[pattern.find('*'):].split(os.path.sep)[0]
         out_det_path = os.path.join(args.img_path, phase, seq, 'det')
         if not os.path.exists(out_det_path):
@@ -63,29 +73,28 @@ if __name__ == "__main__":
             os.makedirs(out_img_dir)
 
         with open(os.path.join(out_det_path, 'det.txt'), 'w') as out_file:
-
+            # for using last frame, add one more time last frame.
             img_id_list = [i+1 for i in range(len(os.listdir(img_dir)))]
             img_id_list.append(img_id_list[-1])
+
+            frame_id = 0
             for img_id in img_id_list:
                 frame_id += 1
                 img_file = '%06d.jpg'%(img_id)
 
                 img_file_path = os.path.join(img_dir, img_file)
-                # print(img_file_path)
                 frame = cv2.imread(img_file_path, cv2.IMREAD_COLOR)
 
+                # inference
                 det_start_time = time.time()
                 res = detector.inference(frame)
                 det_end_time = time.time()
 
                 total_time += det_end_time - det_start_time
                 total_frame += 1
-
+                
                 before_frame_id = frame_id - 1
                 for object_dict in res:
-                    if not object_dict['class_id'] == 0:
-                        continue
-
                     d = detector.to_dets(object_dict)
 
                     print('%d,-1,%d,%d,%d,%d,1,-1,-1,-1'\
