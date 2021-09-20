9d369fb40d72af333ec4751bc7cfb7407c952919
MilyangParkJaeHun <jh9277@naver.com>
2021-09-20 11:18:20 +0000

    Create model downloads file

---
+++ IR/Ssd/model_downloads.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oLK1wMX9NPX5yF9gz_8tGFi5hSR8QGBz' -O ssd_coco.tar.xz
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1o2NOTWZ12YaCvOO6chg4cIg04D3Kl_hL' -O ssdlite_coco.tar.xz
+
---
+++ IR/Yolo/model_downloads.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1B6tbajDlvqSAirmTIw8Iyc5ch-jhY56F' -O yolov4-tiny_coco.tar.xz
+
