74558363c3ffc283da67b225d823b9296263d40d
MilyangParkJaeHun <jh9277@naver.com>
2021-09-21 14:08:53 +0000

    Decompress and remove tar.xz file

--- IR/Ssd/model_downloads.sh
+++ IR/Ssd/model_downloads.sh
@@ -3,3 +3,8 @@
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oLK1wMX9NPX5yF9gz_8tGFi5hSR8QGBz' -O ssd_coco.tar.xz
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1o2NOTWZ12YaCvOO6chg4cIg04D3Kl_hL' -O ssdlite_coco.tar.xz
 
+tar -xvf ssd_coco.tar.xz
+tar -xvf ssdlite_coco.tar.xz
+rm ssd_coco.tar.xz
+rm ssdlite_coco.tar.xz
+
